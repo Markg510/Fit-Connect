f0e55adbc1cf1aca11b003a5ad9fe26e3d8e7a5d
Mark George <prontocoding5133@gmail.com>
2025-08-27 12:09:20 +0300

    Initial Commit

---
+++ Fit Connect/Fit_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fit_ConnectApp.swift
+//  Fit Connect
+//
+//  Created by Mark George on 27/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Fit_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
